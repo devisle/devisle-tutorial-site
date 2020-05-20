9d18ad85b7b3362fd43880c236c0c24671583ee2
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-05-20 17:52:47 +0530

    Added some basic styling. Need to change

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-
+import "./styles.scss";
 /**
  * Generic App component which receives a component and pageProps
  * @author shreyas1307
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,38 +1,79 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
 
 /**
  * Initial Routes for the Front-end
+ *
  * @author shreyas1307
  */
 
 export default function Home() {
+	const [categories, setCategories] = useState([
+		{ icon: "devicon-javascript-plain", categoryName: "JavaScript" },
+		{ icon: "devicon-python-plain", categoryName: "Python" },
+	]);
 	return (
 		<div className="container">
 			<Head>
 				<title>Dev Isle Tutorials</title>
 				<link rel="icon" href="/favicon.ico" />
+				<link
+					rel="stylesheet"
+					href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
+				></link>
 			</Head>
 
 			<main>
-				<ul>
-					<li>
-						<Link
-							as="/javascript/variables"
-							href="/[category]/[tutorial]"
+				<header>
+					<div className="navbar">
+						<div className="nav-title">Dev Isle Tutorials</div>
+						{/* {user?.isAuthenticated ? (
+							<div className="nav-items">Welcome</div>
+						) : ( */}
+						<div className="nav-items">Login</div>
+						{/* )} */}
+					</div>
+				</header>
+				<main>
+					<div className="main">
+						<div className="w-75">
+							<div>
+								<input
+									type="search"
+									className="tutorial-search"
+									placeholder="Search a tutorial..."
+								/>
+							</div>
+							<div>
+								<div>Categories</div>
+							</div>
+							<div className="category-list">
+								{categories.map((categ, index) => {
+									return (
+										<div
+											key={index}
+											className="category-item"
+										>
+											<div>{categ.categoryName}</div>
+											<i className={categ.icon}></i>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+						<div
+							className="w-25"
+							style={{
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
 						>
-							JavaScript Variables
-						</Link>
-					</li>
-					<li>
-						<Link
-							as="/javascript/functions"
-							href="/[category]/[tutorial]"
-						>
-							JavaScript functions
-						</Link>
-					</li>
-				</ul>
+							Ads
+						</div>
+					</div>
+				</main>
 			</main>
 		</div>
 	);
