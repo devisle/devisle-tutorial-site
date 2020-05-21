5a8ad4df74ca4e5d3bf2883b8ab4735a3907a6bc
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-05-21 21:33:17 +0100

    NodeJS.Global JSDoc type format

--- backend/typings/nodejs/index.d.ts
+++ backend/typings/nodejs/index.d.ts
@@ -2,27 +2,18 @@
  * NodeJS GLOBAL type extension
  * This is used for adding Jest globals, 
  * so that our *.unit/int.test.ts can pickup on the globals
- * We're simply unable to otherwise...
  * 
  * @author ale8k
  */
 declare module NodeJS {
     interface Global {
-        /**
-         * Pre-determined by our @shelf/jest-mongodb preset
-         */
+        /** Pre-determined by our @shelf/jest-mongodb preset */
         __MONGO_URI__: string;
-        /**
-         * Pre-determined by our @shelf/jest-mongodb preset
-         */
+        /** Pre-determined by our @shelf/jest-mongodb preset */
         __MONGO_DB_NAME__: string;
-        /**
-         * Specifically specified URI to test on
-         */
+        /** Specifically specified URI to test on */
         __SPECIFIC__MONGO_URI__: string;
-        /**
-         * Specific specified DB name to test on
-         */
+        /** Specific specified DB name to test on */
         __SPECIFIC__MONGO_DB_NAME__: string;
     }
 }
