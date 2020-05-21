c32919b5c9bd2eebc2ea19fc49d84b2aabb966d7
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-05-21 15:37:25 +0530

    Removed yarn.lock, testing different data fetching

--- frontend/pages/[category]/[tutorial].tsx
+++ frontend/pages/[category]/[tutorial].tsx
@@ -1,18 +1,36 @@
 import { useRouter } from "next/router";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
 
 /**
  * Example of a Dynamic route
  * folder or a file wrapped like [name] indicates it as a dynamic route.
  * Eg: If the main route is [category] sub route [tutorial] then the route query params for
  * /javascript/variables will be {category: 'javascript', tutorial: 'variables}
- * 
+ *
  * @author shreyas1307
  */
-export default function Tutorial(): JSX.Element {
-	const router = useRouter();
-	return (
-		<h2>
-			{router.query.category} - {router.query.tutorial}
-		</h2>
-	);
+export default function Tutorial(props): JSX.Element {
+    const router = useRouter();
+    console.log("props here in component", props);
+    return (
+        <h2>
+            {router.query.category} - {router.query.tutorial}
+        </h2>
+    );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+    ctx: GetServerSidePropsContext
+) => {
+    console.log(ctx.params, "ctx.params");
+    console.log(ctx.preview, "ctx.preview");
+    console.log(ctx.previewData, "ctx.previewData");
+    console.log(ctx.query, "ctx.query");
+    // console.log(ctx.req, "ctx.req");
+    // console.log(ctx.res, "ctx.res");
+    return {
+        props: {
+            query: { ...ctx.params },
+        },
+    };
+};
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import { useState } from "react";
+import { GetStaticProps, GetStaticPropsContext } from "next";
 
 /**
  * Initial Routes for the Front-end
@@ -8,72 +9,83 @@ import { useState } from "react";
  * @author shreyas1307
  */
 export default function index(props) {
-	const [categories, setCategories] = useState([
-		{ icon: "devicon-javascript-plain", categoryName: "JavaScript" },
-		{ icon: "devicon-python-plain", categoryName: "Python" },
-	]);
-	return (
-		<div className="container">
-			<Head>
-				<title>Dev Isle Tutorials</title>
-				<link rel="icon" href="/favicon.ico" />
-				<link
-					rel="stylesheet"
-					href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
-				></link>
-			</Head>
+    const [categories, setCategories] = useState([
+        { icon: "devicon-javascript-plain", categoryName: "JavaScript" },
+        { icon: "devicon-python-plain", categoryName: "Python" },
+    ]);
+    return (
+        <div className="container">
+            <Head>
+                <title>Dev Isle Tutorials</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link
+                    rel="stylesheet"
+                    href="https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css"
+                ></link>
+            </Head>
 
-			<main>
-				<header>
-					<div className="navbar">
-						<div className="nav-title">Dev Isle Tutorials</div>
-						{/* {user?.isAuthenticated ? (
+            <main>
+                <header>
+                    <div className="navbar">
+                        <div className="nav-title">Dev Isle Tutorials</div>
+                        {/* {user?.isAuthenticated ? (
 							<div className="nav-items">Welcome</div>
 						) : ( */}
-						<div className="nav-items">Login</div>
-						{/* )} */}
-					</div>
-				</header>
-				<main>
-					<div className="main">
-						<div className="w-75">
-							<div>
-								<input
-									type="search"
-									className="tutorial-search"
-									placeholder="Search a tutorial..."
-								/>
-							</div>
-							<div>
-								<div>Categories</div>
-							</div>
-							<div className="category-list">
-								{categories.map((categ, index) => {
-									return (
-										<div
-											key={index}
-											className="category-item"
-										>
-											<div>{categ.categoryName}</div>
-											<i className={categ.icon}></i>
-										</div>
-									);
-								})}
-							</div>
-						</div>
-						<div
-							className="w-25"
-							style={{
-								display: "flex",
-								alignItems: "center",
-								justifyContent: "center",
-							}}
-						>
-							Ads
-						</div>
-					</div>
-				</main>
-			</main>
-		</div>
-	);
+                        <div className="nav-items">Login</div>
+                        {/* )} */}
+                    </div>
+                </header>
+                <main>
+                    <div className="main">
+                        <div className="w-75">
+                            <div>
+                                <input
+                                    type="search"
+                                    className="tutorial-search"
+                                    placeholder="Search a tutorial..."
+                                />
+                            </div>
+                            <div>
+                                <div>Categories</div>
+                            </div>
+                            <div className="category-list">
+                                {categories.map((categ, index) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            className="category-item"
+                                        >
+                                            <div>{categ.categoryName}</div>
+                                            <i className={categ.icon}></i>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                        <div
+                            className="w-25"
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            Ads
+                        </div>
+                    </div>
+                </main>
+            </main>
+        </div>
+    );
 }
+
+export const getStaticProps: GetStaticProps = async (
+    ctx: GetStaticPropsContext
+) => {
+    console.log(ctx.params);
+    console.log(ctx.preview);
+    console.log(ctx.previewData);
+    return {
+        props: {},
+    };
+};
