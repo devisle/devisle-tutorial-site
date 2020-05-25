c75dc6a44aa078c30b207bd2dc21446a6b58e4de
Alexander Kilroy (ale8k) <alexanderkilroy@outlook.com>
2020-05-25 21:05:24 +0100

    Further response endpoint tests
    More LoginController tests.

--- backend/src/controllers/__tests__/CMSLoginController.unit.test.ts
+++ backend/src/controllers/__tests__/CMSLoginController.unit.test.ts
@@ -1,6 +1,7 @@
 import Server from "../../App";
 import supertest from "supertest";
 import { MongoClient, Db, Collection } from "mongodb";
+import { UNAUTHORISED_TEXT, BAD_REQUEST_TEXT } from "../../constants";
 
 /**
  * This test utilises [supertest]{@link https://www.npmjs.com/package/supertest}
@@ -38,30 +39,66 @@ describe("CMSLoginController", () => {
         );
     });
 
-    it("should login successfully, with the correct response", async (done) => {
+    /**
+     * /CMS/AUTH/LOGIN
+     */
+    it("/cms/auth/login should login successfully, with 200 response", async (done) => {
         await new Server({ path: ".env.testing" }).setupServer().then((app) => {
             supertest(app).post("/cms/auth/login").send({ username: "alex", password: "p123" })
-            .expect(200)
-            .expect(response => {
-                expect(response.body.successfulLogin).toBe(true);
-            }).end(done);
+                .expect(200)
+                .expect(response => {
+                    expect(response.body.successfulLogin).toBe(true);
+                }).end(done);
         });
     });
 
-    it("should confirm the user is logged in according to their JWT", async (done) => {
+    it("/cms/auth/login should login unsuccessfully, with 401 response", async (done) => {
+        await new Server({ path: ".env.testing" }).setupServer().then((app) => {
+            supertest(app).post("/cms/auth/login").send({ username: "alex", password: "p1233" })
+                .expect(401)
+                .expect(response => {
+                    expect(response.text).toBe(UNAUTHORISED_TEXT);
+                }).end(done);
+        });
+    });
+
+    it("/cms/auth/login should fail login procedure due to wrong credential format, with 400 response", async (done) => {
+        await new Server({ path: ".env.testing" }).setupServer().then((app) => {
+            supertest(app).post("/cms/auth/login").send({ username: "alex", passwordd: "p1233" })
+                .expect(400)
+                .expect(response => {
+                    expect(response.text).toBe(BAD_REQUEST_TEXT);
+                }).end(done);
+        });
+    });
+
+    /**
+     * /CMS/AUTH/CONFIRM
+     */
+    it("/cms/auth/confirm should confirm the user is logged in by returning name, with 200 response", async (done) => {
         await new Server({ path: ".test.env" }).setupServer().then((app) => {
             let jwt: string;
             const agent = supertest(app);
             // Grab our JWT by logging in
             agent.post("/cms/auth/login").send({ username: "alex", password: "p123" })
-            // Utilise supertest's callback to pass the JWT
-            .expect(response => {
-                jwt = response.body.jwt;
-            }).end(() => {
-                agent.get("/cms/auth/confirm").set("Authorization", `Bearer ${jwt}`).expect(response => {
-                    expect(response.body.username).toBe("alex");
-                }).end(done);
-            });
+                // Utilise supertest's callback to pass the JWT
+                .expect(response => {
+                    jwt = response.body.jwt;
+                }).end(() => {
+                    agent.get("/cms/auth/confirm").set("Authorization", `Bearer ${jwt}`).expect(200).expect(response => {
+                        expect(response.body.username).toBe("alex");
+                    }).end(done);
+                });
+        });
+    });
+
+    it("/cms/auth/confirm should come back unauthorised, with 401 response", async (done) => {
+        await new Server({ path: ".test.env" }).setupServer().then((app) => {
+            const jwt: string = "e";
+            const agent = supertest(app);
+            agent.get("/cms/auth/confirm").set("Authorization", `Bearer ${jwt}`).expect(401).expect(response => {
+                expect(response.text).toBe(UNAUTHORISED_TEXT);
+            }).end(done);
         });
     });
 
