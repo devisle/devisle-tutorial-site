0cb3c41b52d8fc6196f9a011b654c08171e030e4
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-05-31 22:05:41 +0530

    Wrote a test for getSpecifiedCategories endpoint

--- backend/src/controllers/__tests__/PublicTutorialController.unit.test.ts
+++ backend/src/controllers/__tests__/PublicTutorialController.unit.test.ts
@@ -12,7 +12,7 @@ import { BAD_OBJECTID_PARSE_TEXT } from "../../constants";
  * This test utilises [supertest]{@link https://www.npmjs.com/package/supertest}
  * and initialises the full server to perform the test per test.
  *
- * @author ale8k
+ * @author ale8k, shreyas1307
  */
 describe("PublicTutorialController", () => {
     let connection: MongoClient;
@@ -21,22 +21,25 @@ describe("PublicTutorialController", () => {
         log.setDefaultLevel("silent");
         JestHelper.setupTestHTTPEnv();
 
-        connection = await MongoClient.connect((global.__MONGO_URI__), {
+        connection = await MongoClient.connect(global.__MONGO_URI__, {
             poolSize: 10,
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         });
 
-        PublicTutorialService.db = await connection.db(global.__MONGO_DB_NAME__);
+        PublicTutorialService.db = await connection.db(
+            global.__MONGO_DB_NAME__
+        );
 
         await PublicTutorialService.db.collection("tutorials").insertOne({
             name: "another name",
-            html: "somestuff that won't be considered     <p>--## Section 1</p>  <p>So this is my section, I'm talking crap here... <p>--## Section 2</p> more crap",
+            html:
+                "somestuff that won't be considered     <p>--## Section 1</p>  <p>So this is my section, I'm talking crap here... <p>--## Section 2</p> more crap",
             markdown: "",
             category: "javascript",
             authorId: "5ec17321f63b3c281463fd2a",
             authorName: "alex",
-            isAvailable: true
+            isAvailable: true,
         });
     });
 
@@ -44,42 +47,60 @@ describe("PublicTutorialController", () => {
      * /PUBLIC/TUTORIALS/{TUTID}
      */
     it("/public/tutorials/{tutid} should return the parsed tutorial, with a 200 response", (done) => {
-        PublicTutorialService.db.collection("tutorials").find({}).toArray().then(resp => {
-            const lastAdded: ITutorial = resp[resp.length - 1];
-            new App({ path: ".test.env" }).setupServer().then((app) => {
-                const agent = supertest(app);
-                agent.get(`/public/tutorials/${lastAdded._id}`)
-                    .expect(200)
-                    .expect(response => {
-                        const tut: PublicTutorial = response.body;
-                        expect(tut).not.toEqual(null);
-                        expect(tut.content).toHaveLength(2);
-                        expect(tut.content[0].sectionTitle).toEqual("Section 1");
-                        expect(tut.content[0].sectionBody).toEqual("<p>So this is my section, I'm talking crap here...");
-                        expect(tut.content[1].sectionTitle).toEqual("Section 2");
-                        expect(tut.content[1].sectionBody).toEqual("more crap");
-                        expect(Object.keys(tut).length).toEqual(5);
-                    }).end(done);
+        PublicTutorialService.db
+            .collection("tutorials")
+            .find({})
+            .toArray()
+            .then((resp) => {
+                const lastAdded: ITutorial = resp[resp.length - 1];
+                new App({ path: ".test.env" }).setupServer().then((app) => {
+                    const agent = supertest(app);
+                    agent
+                        .get(`/public/tutorials/${lastAdded._id}`)
+                        .expect(200)
+                        .expect((response) => {
+                            const tut: PublicTutorial = response.body;
+                            expect(tut).not.toEqual(null);
+                            expect(tut.content).toHaveLength(2);
+                            expect(tut.content[0].sectionTitle).toEqual(
+                                "Section 1"
+                            );
+                            expect(tut.content[0].sectionBody).toEqual(
+                                "<p>So this is my section, I'm talking crap here..."
+                            );
+                            expect(tut.content[1].sectionTitle).toEqual(
+                                "Section 2"
+                            );
+                            expect(tut.content[1].sectionBody).toEqual(
+                                "more crap"
+                            );
+                            expect(Object.keys(tut).length).toEqual(5);
+                        })
+                        .end(done);
+                });
             });
-        });
     });
 
     it("/public/tutorials/{tutid} should return bad objectid parse message, with a 400 response", (done) => {
         new App({ path: ".test.env" }).setupServer().then((app) => {
             const agent = supertest(app);
-            agent.get(`/public/tutorials/01223`)
+            agent
+                .get(`/public/tutorials/01223`)
                 .expect(400)
-                .expect(response => {
+                .expect((response) => {
                     expect(response.text).toEqual(BAD_OBJECTID_PARSE_TEXT);
-                }).end(done);
+                })
+                .end(done);
         });
     });
 
     it("/public/tutorials/{tutid} should return a 204 response", (done) => {
         new App({ path: ".test.env" }).setupServer().then((app) => {
             const agent = supertest(app);
-            agent.get(`/public/tutorials/000000000000000000000000`)
-                .expect(204).end(done);
+            agent
+                .get(`/public/tutorials/000000000000000000000000`)
+                .expect(204)
+                .end(done);
         });
     });
 
@@ -89,28 +110,52 @@ describe("PublicTutorialController", () => {
     it("/public/tutorials/cards/{category} should return the parsed tutorial, with a 200 response", (done) => {
         new App({ path: ".test.env" }).setupServer().then((app) => {
             const agent = supertest(app);
-            agent.get(`/public/tutorials/cards/javascripT`)
+            agent
+                .get(`/public/tutorials/cards/javascripT`)
                 .expect(200)
-                .expect(response => {
+                .expect((response) => {
                     expect(response.body).not.toEqual(null);
-                    expect(response.body[0].cardText).toEqual("Section 1  So this is my section, I'm talking crap here...  Section 2 more crap");
-                }).end(done);
+                    expect(response.body[0].cardText).toEqual(
+                        "Section 1  So this is my section, I'm talking crap here...  Section 2 more crap"
+                    );
+                })
+                .end(done);
         });
     });
 
     it("/public/tutorials/cards/{category} should return 404, because the given category does not exist", (done) => {
         new App({ path: ".test.env" }).setupServer().then((app) => {
             const agent = supertest(app);
-            agent.get(`/public/tutorials/cards/javascripT-a-none-existent-category`)
-                .expect(404).end(done);
+            agent
+                .get(
+                    `/public/tutorials/cards/javascripT-a-none-existent-category`
+                )
+                .expect(404)
+                .end(done);
         });
     });
 
     it("/public/tutorials/cards/{category}?offset=*&outset=* should return 400, because the given values aren't integers", (done) => {
         new App({ path: ".test.env" }).setupServer().then((app) => {
             const agent = supertest(app);
-            agent.get(`/public/tutorials/cards/javascripT?offset=bob&outset=harry`)
-                .expect(400).end(done);
+            agent
+                .get(
+                    `/public/tutorials/cards/javascripT?offset=bob&outset=harry`
+                )
+                .expect(400)
+                .end(done);
+        });
+    });
+
+    it("public/tutorials/categories/query?offset=*&outset=* should return 400, because the given values aren't integers", (done) => {
+        new App({ path: ".test.env" }).setupServer().then((app) => {
+            const agent = supertest(app);
+            agent
+                .get(
+                    `/public/tutorials/categories/query?offset="69"&outset="90"`
+                )
+                .expect(400)
+                .end(done);
         });
     });
 
