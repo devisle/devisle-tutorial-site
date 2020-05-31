f3aea2d41a2e77494ac7755d06d7bf6dab977a90
Shreyas Balachandran <shreyas.balachandran@outlook.com>
2020-05-31 22:05:27 +0530

    Created a controller for getSpecifiedCategories

--- backend/src/controllers/PublicTutorialController.ts
+++ backend/src/controllers/PublicTutorialController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 import PublicTutorialService from "../services/PublicTutorialService";
 import { ObjectId, MongoError } from "mongodb";
-import { BAD_OBJECTID_PARSE_TEXT, INTERNAL_ERROR_TEXT, TUTORIAL_CATEGORIES } from "../constants";
+import {
+    BAD_OBJECTID_PARSE_TEXT,
+    INTERNAL_ERROR_TEXT,
+    TUTORIAL_CATEGORIES,
+} from "../constants";
 
 /**
  * PUBLIC Tutorial route controller
  *
- * @author ale8k
+ * @author ale8k, shreyas1307
  */
 export default class PublicTutorialController {
     /**
@@ -20,7 +24,10 @@ export default class PublicTutorialController {
     public static getTutorialById(req: Request, res: Response): void {
         try {
             const tutObjectId = new ObjectId(req.params.tutId);
-            PublicTutorialService.getPublicTutById("tutorials", tutObjectId).then(
+            PublicTutorialService.getPublicTutById(
+                "tutorials",
+                tutObjectId
+            ).then(
                 (response) => {
                     if (response === null) {
                         res.sendStatus(204).end();
@@ -28,7 +35,11 @@ export default class PublicTutorialController {
                         res.status(200).send(response).end();
                     }
                 },
-                (err: MongoError) =>  res.status(500).send(INTERNAL_ERROR_TEXT + JSON.stringify(err)).end()
+                (err: MongoError) =>
+                    res
+                        .status(500)
+                        .send(INTERNAL_ERROR_TEXT + JSON.stringify(err))
+                        .end()
             );
         } catch (e) {
             res.status(400).send(BAD_OBJECTID_PARSE_TEXT).end();
@@ -53,23 +64,25 @@ export default class PublicTutorialController {
     public static getSpecifiedTutorialCards(req: Request, res: Response): void {
         const category = req.params.category.toLowerCase().replace("-", " ");
         if (PublicTutorialController.verifyCategory(category)) {
-            PublicTutorialService.getTutCardsInCategory("tutorials", category).then(
-                (cards) => {
-                    if (req.query.offset && req.query.outset) {
-                        const offset: number = parseInt(req.query.offset);
-                        const outset: number = parseInt(req.query.outset);
+            PublicTutorialService.getTutCardsInCategory(
+                "tutorials",
+                category
+            ).then((cards) => {
+                if (req.query.offset && req.query.outset) {
+                    const offset: number = parseInt(req.query.offset);
+                    const outset: number = parseInt(req.query.outset);
 
-                        if (Number.isInteger(offset) && Number.isInteger(outset)) {
-                            res.status(200).send(cards.splice(offset, outset));
-                        } else {
-                            res.status(400).send("Query params for the category request must be integers.");
-                        }
-
+                    if (Number.isInteger(offset) && Number.isInteger(outset)) {
+                        res.status(200).send(cards.splice(offset, outset));
                     } else {
-                        res.send(cards);
+                        res.status(400).send(
+                            "Query params for the category request must be integers."
+                        );
                     }
+                } else {
+                    res.send(cards);
                 }
-            );
+            });
         } else {
             res.status(404).send("The given category does not exist.");
         }
@@ -94,8 +107,8 @@ export default class PublicTutorialController {
      * @param {Response} res our res obj
      */
     public static getAllPaths(req: Request, res: Response): void {
-        PublicTutorialService.getOnlyTutNamesAndIds("tutorials").then(
-            (paths) => res.status(200).send(paths)
+        PublicTutorialService.getOnlyTutNamesAndIds("tutorials").then((paths) =>
+            res.status(200).send(paths)
         );
     }
 
@@ -111,4 +124,20 @@ export default class PublicTutorialController {
         return false;
     }
 
+    public static getSpecifiedCategories(req: Request, res: Response): void {
+        const offset: number = parseInt(req.query.offset);
+        const outset: number = parseInt(req.query.outset);
+
+        if (!offset || !outset) {
+            res.status(400).send(
+                "This endpoint requires query parameters, offset and outset, of type integer."
+            );
+        } else if (Number.isInteger(offset) && Number.isInteger(outset)) {
+            res.status(200).send(TUTORIAL_CATEGORIES.slice(offset, outset));
+        } else {
+            res.status(400).send(
+                "Query params for the category request must be integers."
+            );
+        }
+    }
 }
